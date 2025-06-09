f28a914086b75d1bc44be76f3edda92bb4412972
Ali Usman <L1S22BSSE0074@ucp.edu.pk>
2025-06-09 18:28:14 +0500

    Update: Learned Complex State: Arrays

---
+++ S3_react-states/3.2-props-vs-state/src/ComplexState.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import './complexState.css';
+
+export default function ComplexState() {
+	// const myFavoriteThings = [];
+	const [myFavoriteThings, setMyFavoriteThings] = useState([]);
+	const allFavoriteThings = [
+		'💦🌹',
+		'😺',
+		'💡🫖',
+		'🔥🧤',
+		'🟤🎁',
+		'🐴',
+		'🍎🥧',
+		'🚪🔔',
+		'🛷🔔',
+		'🥩🍝',
+	];
+	const thingsElements = myFavoriteThings.map((thing) => (
+		<p key={thing}>{thing}</p>
+	));
+
+	function addFavoriteThing() {
+		// myFavoriteThings.push('Test'); // will not update UI because we are modifying it with non-state function(modifying state variable direclty does not trigger re-render in react)
+
+		//Info: Beside the above fact, it is hard and fast rule in react, never ever modify state direclty as it is destructive action i.e `push()` [it modifies the orignal array] just like `count++`
+
+		// console.log(myFavoriteThings);
+
+		// setMyFavoriteThings((prevFavThings) => prevFavThings.push('Test')); // will also not work as `prevFavThimgs` is actually a reference to orignal array `myFavoriteThings`
+
+		// solution? return a brand new array
+		// setMyFavoriteThings((prevFavThings) => [...prevFavThings, 'Test']);
+
+		setMyFavoriteThings((prevFavThings) => [
+			...prevFavThings,
+			allFavoriteThings[prevFavThings.length],
+		]);
+	}
+
+	function removeFavoriteThing() {
+		setMyFavoriteThings((prevFavThings) => {
+			let newArr = [...prevFavThings];
+			newArr.pop();
+			return newArr;
+		});
+	}
+
+	return (
+		<main>
+			<button onClick={addFavoriteThing}>Add item</button>
+			<button onClick={removeFavoriteThing}>Remove item</button>
+			<section aria-live="polite">{thingsElements}</section>
+		</main>
+	);
+}
--- S3_react-states/3.2-props-vs-state/src/main.jsx
+++ S3_react-states/3.2-props-vs-state/src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import IntroToState from './IntroToState.jsx';
+import ComplexState from './ComplexState.jsx';
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
-		<IntroToState />
+		{/* <IntroToState /> */}
+		<ComplexState />
 	</StrictMode>
 );
