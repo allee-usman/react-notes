1670e4661888ac366c5917ada4321790d13a6dc5
Ali Usman <L1S22BSSE0074@ucp.edu.pk>
2025-06-09 12:27:52 +0500

    Feature Update: Add html, css and basic from handling for Add New Ingredient

--- S3_react-states/chef-claude-project/src/components/Main.jsx
+++ S3_react-states/chef-claude-project/src/components/Main.jsx
@@ -1,17 +1,46 @@
 function Main() {
+	let ingredients = [
+		'Oregano',
+		'Basil',
+		'Garlic',
+		'Olive Oil',
+		'Salt',
+		'Pepper',
+		'Tomato Sauce',
+		'Chicken Broth',
+	];
+
+	const ingredientList = ingredients.map((ingredient) => {
+		return <li key={ingredient}>{ingredient}</li>;
+	});
+
+	function handleSubmit(event) {
+		event.preventDefault();
+
+		const formData = new FormData(event.currentTarget);
+		const newIngredient = formData.get('ingredient-field').trim();
+
+		ingredients.push(newIngredient);
+		console.log(ingredients);
+	}
+
 	return (
 		<main>
-			<form action="#">
+			<form action="#" onSubmit={handleSubmit}>
 				<input
 					type="text"
-					name="input-field"
-					id="text-iput"
+					name="ingredient-field"
+					id="ingredient-input"
 					placeholder="e.g. oregano"
 					aria-label="Add ingredient"
 					required
 				/>
 				<button type="submit">+ Add Ingredient</button>
 			</form>
+			<section className="ingredients-list-container">
+				<h2>Ingredients on hnad</h2>
+				<ul className="ingredients-list">{ingredientList}</ul>
+			</section>
 		</main>
 	);
 }
