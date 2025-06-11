fe64671671384be52e6043984be86f16f119fabd
Ali Usman <L1S22BSSE0074@ucp.edu.pk>
2025-06-11 10:10:11 +0500

    Update: Add functionality to Get a recipe button to toggle placeholder

--- S3_react-states/chef-claude-project/src/components/Main.jsx
+++ S3_react-states/chef-claude-project/src/components/Main.jsx
@@ -22,6 +22,11 @@ function Main() {
 	}
 	const showIngredientSec = ingredients.length ? true : false;
 	// console.log(showIngredientSec);
+	const [recipeShown, setRecipeShown] = useState(false);
+
+	function toggleShowRecipe() {
+		setRecipeShown((prevShowStatus) => !prevShowStatus);
+	}
 
 	return (
 		<main>
@@ -46,39 +51,41 @@ function Main() {
 								<h2>Ready for a recipe?</h2>
 								<p>Generate a recipe from your list of ingredients.</p>
 							</div>
-							<button>Get a recipe</button>
+							<button onClick={toggleShowRecipe}>Get a recipe</button>
 						</div>
 					)}
 				</section>
 			)}
-			<section className="suggested-recipe-container">
-				<h2>Suggested Recipe</h2>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
-					dolor quis nulla id, excepturi reprehenderit maiores repellat dolores
-					tempore quidem inventore quos fugiat dolore natus nobis tenetur
-					voluptate in possimus cupiditate at nemo eum. Facere veniam, laborum,
-					labore quos asperiores libero eaque sequi vitae quas minima
-					recusandae, necessitatibus nam illum?
-				</p>
-				<h3>Ingredients:</h3>
-				<ul className="list">
-					<li>Ingredient 1</li>
-					<li>Ingredient 2</li>
-					<li>Ingredient 3</li>
-					<li>Ingredient 4</li>
-					<li>Ingredient 5</li>
-				</ul>
-				<h3>Instructions:</h3>
-				<ol className="list">
-					<li>Instruction 1</li>
-					<li>Instruction 2</li>
-					<li>Instruction 3</li>
-					<li>Instruction 4</li>
-					<li>Instruction 5</li>
-				</ol>
-				<p>Enjoy!</p>
-			</section>
+			{recipeShown && (
+				<section className="suggested-recipe-container">
+					<h2>Suggested Recipe</h2>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+						dolor quis nulla id, excepturi reprehenderit maiores repellat
+						dolores tempore quidem inventore quos fugiat dolore natus nobis
+						tenetur voluptate in possimus cupiditate at nemo eum. Facere veniam,
+						laborum, labore quos asperiores libero eaque sequi vitae quas minima
+						recusandae, necessitatibus nam illum?
+					</p>
+					<h3>Ingredients:</h3>
+					<ul className="list">
+						<li>Ingredient 1</li>
+						<li>Ingredient 2</li>
+						<li>Ingredient 3</li>
+						<li>Ingredient 4</li>
+						<li>Ingredient 5</li>
+					</ul>
+					<h3>Instructions:</h3>
+					<ol className="list">
+						<li>Instruction 1</li>
+						<li>Instruction 2</li>
+						<li>Instruction 3</li>
+						<li>Instruction 4</li>
+						<li>Instruction 5</li>
+					</ol>
+					<p>Enjoy!</p>
+				</section>
+			)}
 		</main>
 	);
 }
