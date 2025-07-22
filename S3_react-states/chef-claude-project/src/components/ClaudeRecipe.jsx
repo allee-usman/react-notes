d8f89fa98dbb1a2b439140c4a0ac1a5a7c4c979a
ali usman <l1s22bsse0074@ucp.edu.pk>
2025-07-23 00:29:51 +0500

    Refactor Project into different components

---
+++ S3_react-states/chef-claude-project/src/components/ClaudeRecipe.jsx
@@ -0,0 +1,32 @@
+export default function Recipe() {
+	return (
+		<section className="suggested-recipe-container">
+			<h2>Suggested Recipe</h2>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+				dolor quis nulla id, excepturi reprehenderit maiores repellat dolores
+				tempore quidem inventore quos fugiat dolore natus nobis tenetur
+				voluptate in possimus cupiditate at nemo eum. Facere veniam, laborum,
+				labore quos asperiores libero eaque sequi vitae quas minima recusandae,
+				necessitatibus nam illum?
+			</p>
+			<h3>Ingredients:</h3>
+			<ul className="list">
+				<li>Ingredient 1</li>
+				<li>Ingredient 2</li>
+				<li>Ingredient 3</li>
+				<li>Ingredient 4</li>
+				<li>Ingredient 5</li>
+			</ul>
+			<h3>Instructions:</h3>
+			<ol className="list">
+				<li>Instruction 1</li>
+				<li>Instruction 2</li>
+				<li>Instruction 3</li>
+				<li>Instruction 4</li>
+				<li>Instruction 5</li>
+			</ol>
+			<p>Enjoy!</p>
+		</section>
+	);
+}
---
+++ S3_react-states/chef-claude-project/src/components/IngredientsList.jsx
@@ -0,0 +1,20 @@
+export default function IngredientsList({ ingredients, toggleShowRecipe }) {
+	const ingredientList = ingredients.map((ingredient) => {
+		return <li key={ingredient}>{ingredient}</li>;
+	});
+	return (
+		<section className="ingredients-list-container">
+			<h2>Ingredients on hnad</h2>
+			<ul className="list ingredients-list">{ingredientList}</ul>
+			{ingredients.length > 3 && (
+				<div className="get-recipe-container">
+					<div>
+						<h2>Ready for a recipe?</h2>
+						<p>Generate a recipe from your list of ingredients.</p>
+					</div>
+					<button onClick={toggleShowRecipe}>Get a recipe</button>
+				</div>
+			)}
+		</section>
+	);
+}
