f9eb26b5373ee4609172ac4ab348a0638ad2ec33
ali usman <l1s22bsse0074@ucp.edu.pk>
2025-07-25 14:11:49 +0500

    Add Side-effect demo

--- s4-handling-side-effects/meme-generator/src/components/Main.jsx
+++ s4-handling-side-effects/meme-generator/src/components/Main.jsx
@@ -1,22 +1,56 @@
+import { useState } from 'react';
+
 export default function Main() {
+	const [meme, setMeme] = useState({
+		topText: 'One does not simply',
+		bottomText: 'Walk into Mordor',
+		imageUrl: 'http://i.imgflip.com/1bij.jpg',
+	});
+	const handleChange = (event) => {
+		// console.log('Changed!!');
+		// console.log(event); //object
+		// console.log(event.target.value);
+
+		const { value, name } = event.currentTarget;
+		setMeme((prevMeme) => {
+			return {
+				...prevMeme,
+				[name]: value,
+				//Note: To use `[name]: value` make sure name attribute of input element and key of local state(meme) are same.
+			};
+		});
+	};
+
 	return (
 		<main>
 			<div className="form">
 				<label>
 					Top Text
-					<input type="text" placeholder="One does not simply" name="topText" />
+					<input
+						type="text"
+						placeholder={meme.topText}
+						name="topText"
+						onChange={handleChange}
+						value={meme.topText}
+					/>
 				</label>
 
 				<label>
 					Bottom Text
-					<input type="text" placeholder="Walk into Mordor" name="bottomText" />
+					<input
+						type="text"
+						placeholder={meme.bottomText}
+						name="bottomText"
+						onChange={handleChange}
+						value={meme.bottomText}
+					/>
 				</label>
 				<button>Get a new meme image 🖼️</button>
 			</div>
 			<div className="meme">
-				<img src="http://i.imgflip.com/1bij.jpg" />
-				<span className="top">One does not simply</span>
-				<span className="bottom">Walk into Mordor</span>
+				<img src={meme.imageUrl} />
+				<span className="top">{meme.topText}</span>
+				<span className="bottom">{meme.bottomText}</span>
 			</div>
 		</main>
 	);
