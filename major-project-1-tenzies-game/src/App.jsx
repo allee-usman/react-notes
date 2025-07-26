e0e0a7a7d07f400c60d67298707e98f5ba33876e
ali usman <mrau429040@gmail.com>
2025-07-26 15:15:24 +0500

    Add functioning roll dice button

--- major-project-1-tenzies-game/src/App.jsx
+++ major-project-1-tenzies-game/src/App.jsx
@@ -1,28 +1,37 @@
 import Die from './components/Die';
 import { useState } from 'react';
-const getRandomNumber = () => {
-	return Math.ceil(Math.random() * 6);
-};
-const generateAllNewDice = () => {
-	// const dice = [];
-	// for (let i = 0; i < 10; i++) {
-	// 	dice[i] = getRandomNumber();
-	// }
-	// return dice;
-	return new Array(10).fill(0).map(() => getRandomNumber());
-};
 
 export default function App() {
+	const getRandomNumber = () => {
+		return Math.ceil(Math.random() * 6);
+	};
+	const generateAllNewDice = () => {
+		// const dice = [];
+		// for (let i = 0; i < 10; i++) {
+		// 	dice[i] = getRandomNumber();
+		// }
+		// return dice;
+		return new Array(10).fill(0).map(() => getRandomNumber());
+	};
+
 	const [dice, setDice] = useState(generateAllNewDice());
+
 	// console.log(dice);
 
-	const diceElements = dice.map((num) => {
-		return <Die key={num} value={num} />;
+	const diceElements = dice.map((num, index) => {
+		return <Die key={index} value={num} />;
 	});
+
+	function rollDice() {
+		setDice(generateAllNewDice());
+	}
 
 	return (
 		<main>
 			<div className="dice-container">{diceElements}</div>
+			<button onClick={rollDice} className="roll-dice-btn">
+				Roll
+			</button>
 		</main>
 	);
 }
